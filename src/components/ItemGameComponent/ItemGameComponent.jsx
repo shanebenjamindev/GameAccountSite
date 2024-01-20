270355117d51015bb0fb99d9410f148afcab21f4
Your Name <you@example.com>
2024-01-20 15:33:24 +0700

    added responsive list game layout

--- src/components/ItemGameComponent/ItemGameComponent.jsx
+++ src/components/ItemGameComponent/ItemGameComponent.jsx
@@ -6,10 +6,10 @@ import { WrapperLink } from '../../assets/style/commonStyle';
 export default function ItemGameComponent(props) {
     const { game } = props;
     return (
-        <WrapperItemGameComponent className='col-md-4 my-2 '>
-            <WrapperLink to={`/detail/${game.id}`} >
-                <ItemGameContainer className='p-2 d-md-flex w-100' style={{ border: "2px solid var(--primary-fade)" }}>
-                    <div className='col-md-8 text-left'>
+        <WrapperItemGameComponent className='col-lg-4 col-md-6 my-2 '>
+            <WrapperLink to={`/detail/${game.id}`} className='w-100'>
+                <ItemGameContainer className='p-2 d-flex w-100' style={{ border: "2px solid var(--primary-fade)" }}>
+                    <div className='col-8 text-left'>
                         <h5>
                             {game.name}
                         </h5>
@@ -17,7 +17,7 @@ export default function ItemGameComponent(props) {
                             {game.manufacturer}
                         </div>
                     </div>
-                    <img src={game.imageUrl} className='col-md-4' alt='' />
+                    <img src={game.imageUrl} className='col-4' alt='' />
                 </ItemGameContainer>
             </WrapperLink >
         </WrapperItemGameComponent >
